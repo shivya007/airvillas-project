92e5bc6e7577f609d46a3bba80fefaa81b617a38
Shiv Arora <passionshiv007@gmail.com>
2024-12-05 13:11:18 +0530

    userSchema changes

--- models/user.js
+++ models/user.js
@@ -15,7 +15,8 @@ const userSchema = new Schema({
     },
     googleId: {
         type: String,
-        unique: true,
+        unique: false,
+        sparse: true,
     },
     name: {
         type: String,
